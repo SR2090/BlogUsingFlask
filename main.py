115ce1cf0c9daaaefdf8df8cf7a554abd3c1a499
SR2090 <personalidmk3@gmail.com>
2020-11-14 18:45:41 +0530

    changed .html back to route links dynamic linking

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ def homepage():
     return render_template('index.html')
 
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
 
-@app.route('/index.html')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/post.html')
+@app.route('/post')
 def post():
     return render_template('post.html')
 
 
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
