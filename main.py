7e43a0185f698fe48fd61b80ca28405bc4c018a7
SR2090 <personalidmk3@gmail.com>
2020-11-14 18:13:11 +0530

    Added .html extension to route which is bad

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ def homepage():
     return render_template('index.html')
 
 
-@app.route('/contact')
+@app.route('/contact.html')
 def contact():
     return render_template('contact.html')
 
 
-@app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
 
-@app.route('/post')
+@app.route('/post.html')
 def post():
     return render_template('post.html')
 
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
 
