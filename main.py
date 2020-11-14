73e139a84c4d256d3cecf50a5a047bff5e2b8770
SR2090 <personalidmk3@gmail.com>
2020-11-14 17:37:53 +0530

    added routing for html

--- main.py
+++ main.py
@@ -24,5 +24,10 @@ def post():
     return render_template('post.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
