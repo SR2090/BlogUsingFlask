38ea71995f4090c0fb3919d203c782292b1101de
SR2090 <personalidmk3@gmail.com>
2020-11-15 16:41:53 +0530

    Added DB connector and Contact Form ORM

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template
+# For adding data base
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+# Resource
 # The home page of the blog application
+
+# Database
+# connected to SQL db
+# This will connect to sqlite db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+# We need to connect to mysql data base
+# Adding the password and database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/selfreflection'
+db = SQLAlchemy(app)
+
+# This class will define the db table
+
+
+class Contacts(db.Model):
+    """ 
+    sno, name, email, phone_num, msg, date
+    """
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_num = db.Column(db.String(10), nullable=False)
+    msg = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(12), nullable=False)
 
 
 @app.route('/')
